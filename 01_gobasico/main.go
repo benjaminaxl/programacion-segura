526a5caa11690153e5068a1ce65d8e7bcdf4a01d
Benjamin Axl U. <benja604m@gmail.com>
2024-12-11 13:39:55 +0000

    FORMULARIO

---
+++ 01_gobasico/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+    var mundo = "Mundo ok"
+    const var_const = "constante"
+
+    fmt.Print("hola mundo")
+	fmt.Printf("hola %v\n", mundo)
+	fmt.Println("hello",var_const)
+}
